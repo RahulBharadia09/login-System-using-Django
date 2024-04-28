d0fe5502c1817652ca55f6c24ad729d757cdf47b
RahulBharadia09 <rahulbharadia917@gmail.com>
2024-04-28 20:32:50 +0530

    added signin and sigup function

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('',views.home,name="home"),
     path('signin/',views.signin,name="signin"),
     path('signup/',views.signup,name="signup"),
+    path('success/',views.successLogin,name="success"),
+    
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,13 +1,57 @@
-from django.shortcuts import render
-from django.http import  HttpResponse
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+
 
 # Create your views here.
 def home(req):
+    print("from home")
     return render(req,"authentication/index.html")
 
+def successLogin(req):
+    return render(req,"authentication/success.html")
+
+# create account
+def signup(req):
+    if req.method == "POST":
+        userName = req.POST.get("userName")
+        pass1 = req.POST.get("pass1")
+        print("done")
+        myuser = User.objects.create_user(username=userName,password=pass1)
+        myuser.save()
+        messages.success(req,"Successfully Created")
+        return redirect('signin') 
+    return render(req,"authentication/signup.html")
+
+
+#Login
 def signin(req):
+   
+    if req.method == "POST":
+        userName = req.POST.get("userName")
+        pass1 = req.POST.get("pass1")
+        user1 = authenticate(username = userName , password=pass1 )
+        
+
+        if user1 is not None:
+            
+            login(req,user1)
+            return render(req,"authentication/success.html")
+        else:
+           
+            messages.error(req,"Bad Credential")
+            return redirect("signin")
+        
     return render(req,"authentication/signin.html")
     
 
-def signup(req):
-    return render(req,"authentication/signup.html")
+# logout
+
+def signout(req):
+    logout(req)
+    messages.success("Successfully Logout")
+    return redirect('home')
+
+   
